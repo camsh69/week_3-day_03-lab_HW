8fc98d5a1ed3a8f2c4b5e2f6ac98b90a66a4f485
Campbell Forsyth <camsh69@gmail.com>
2021-12-01 13:25:33 +0000

    order class commit

---
+++ models/order.py
@@ -0,0 +1,8 @@
+class Order():
+
+    def __init__(self, customer_name, order_date, quantity, guitar_name, model_year):
+        self.customer_name = customer_name
+        self. order_date = order_date
+        self.quantity = quantity
+        self.guitar_name = guitar_name
+        self.model_year = model_year
