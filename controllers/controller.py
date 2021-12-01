0a7668a9cdb829f6898f8224208bc52bc31a3eb7
Campbell Forsyth <camsh69@gmail.com>
2021-12-01 20:58:05 +0000

    Extension commit

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,3 +5,7 @@ from models.order_list import orders
 @app.route('/orders')
 def index():
     return render_template('index.html', title="Home", orders=orders)
+
+@app.route('/order/<int:index_number>')
+def order(index_number):
+    return render_template('order.html', title="Orders", order=orders[index_number])
--- models/order_list.py
+++ models/order_list.py
@@ -1,6 +1,6 @@
 from models.order import *
 
-order1 = Order("Eddie Van Halen", "01/12/2021", 1, "Fender Stratocaster", 1962)
+order1 = Order("Ritchie Blackmore", "01/12/2021", 1, "Fender Stratocaster", 1962)
 order2 = Order("Jimmy Page", "25/11/2021", 2, "Gibson Les Paul", 1957)
 order3 = Order("Brian May", "10/11/2021", 1, "Gibson Firebird", 1965 )
 
